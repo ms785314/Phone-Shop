d9eb203946ce5dcc6c27eac92f349ceed7ddb234
ms785314 <ms785314@gmail.com>
2022-09-23 10:37:34 +0600

    reviewer profile modified

--- src/components/ReviewerProfile/ReviewerProfile.js
+++ src/components/ReviewerProfile/ReviewerProfile.js
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 
 const ReviewerProfile = (props) => {
     
-    const [reviews, setReviews] = useState([]);
-    useEffect(()=>{
-        fetch('./reviews.json')
-        .then(res => res.json())
-        .then(data => setReviews(data))
-    },[])
-    console.log(reviews);
+
+    // not working for local data
+    // const [reviews, setReviews] = useState([]);
+    // useEffect(()=>{
+    //     fetch('./reviews.json')
+    //     .then(res => res.json())
+    //     .then(data => setReviews(data))
+    // },[])
+    // console.log(reviews);
+
+    const params = useParams();
     
     return (
-        <div>
-            <h1>Hello From Reviewer Profile:{}</h1>
+        <div className='text-3xl font-semibold mt-6 w-full flex justify-center capitalize'>
+            <h1>will show data for id:{params.id}</h1>
         </div>
     );
 };
